2cc6839255003acc51568374a8da4e4109d5200e
Connor Shea <connor.james.shea@gmail.com>
2019-04-25 20:42:23 -0600

    Customize test adapter template

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,78 +1,78 @@
 import * as vscode from 'vscode';
 import { TestAdapter, TestLoadStartedEvent, TestLoadFinishedEvent, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
 import { Log } from 'vscode-test-adapter-util';
-import { loadFakeTests, runFakeTests } from './fakeTests';
+import { loadRspecTests, runRspecTests } from './rspecTests';
 
 /**
  * This class is intended as a starting point for implementing a "real" TestAdapter.
  * The file `README.md` contains further instructions.
  */
-export class ExampleAdapter implements TestAdapter {
+export class RubyAdapter implements TestAdapter {
 
-	private disposables: { dispose(): void }[] = [];
+  private disposables: { dispose(): void }[] = [];
 
-	private readonly testsEmitter = new vscode.EventEmitter<TestLoadStartedEvent | TestLoadFinishedEvent>();
-	private readonly testStatesEmitter = new vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>();
-	private readonly autorunEmitter = new vscode.EventEmitter<void>();
+  private readonly testsEmitter = new vscode.EventEmitter<TestLoadStartedEvent | TestLoadFinishedEvent>();
+  private readonly testStatesEmitter = new vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>();
+  private readonly autorunEmitter = new vscode.EventEmitter<void>();
 
-	get tests(): vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent> { return this.testsEmitter.event; }
-	get testStates(): vscode.Event<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent> { return this.testStatesEmitter.event; }
-	get autorun(): vscode.Event<void> | undefined { return this.autorunEmitter.event; }
+  get tests(): vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent> { return this.testsEmitter.event; }
+  get testStates(): vscode.Event<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent> { return this.testStatesEmitter.event; }
+  get autorun(): vscode.Event<void> | undefined { return this.autorunEmitter.event; }
 
-	constructor(
-		public readonly workspace: vscode.WorkspaceFolder,
-		private readonly log: Log
-	) {
+  constructor(
+    public readonly workspace: vscode.WorkspaceFolder,
+    private readonly log: Log
+  ) {
 
-		this.log.info('Initializing example adapter');
+    this.log.info('Initializing Ruby adapter');
 
-		this.disposables.push(this.testsEmitter);
-		this.disposables.push(this.testStatesEmitter);
-		this.disposables.push(this.autorunEmitter);
+    this.disposables.push(this.testsEmitter);
+    this.disposables.push(this.testStatesEmitter);
+    this.disposables.push(this.autorunEmitter);
 
-	}
+  }
 
-	async load(): Promise<void> {
+  async load(): Promise<void> {
 
-		this.log.info('Loading example tests');
+    this.log.info('Loading Ruby tests');
 
-		this.testsEmitter.fire(<TestLoadStartedEvent>{ type: 'started' });
+    this.testsEmitter.fire(<TestLoadStartedEvent>{ type: 'started' });
 
-		const loadedTests = await loadFakeTests();
+    const loadedTests = await loadRspecTests();
 
-		this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: loadedTests });
+    this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: loadedTests });
 
-	}
+  }
 
-	async run(tests: string[]): Promise<void> {
+  async run(tests: string[]): Promise<void> {
 
-		this.log.info(`Running example tests ${JSON.stringify(tests)}`);
+    this.log.info(`Running Ruby tests ${JSON.stringify(tests)}`);
 
-		this.testStatesEmitter.fire(<TestRunStartedEvent>{ type: 'started', tests });
+    this.testStatesEmitter.fire(<TestRunStartedEvent>{ type: 'started', tests });
 
-		// in a "real" TestAdapter this would start a test run in a child process
-		await runFakeTests(tests, this.testStatesEmitter);
+    // in a "real" TestAdapter this would start a test run in a child process
+    await runRspecTests(tests, this.testStatesEmitter);
 
-		this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: 'finished' });
+    this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: 'finished' });
 
-	}
+  }
 
 /*	implement this method if your TestAdapter supports debugging tests
-	async debug(tests: string[]): Promise<void> {
-		// start a test run in a child process and attach the debugger to it...
-	}
+  async debug(tests: string[]): Promise<void> {
+    // start a test run in a child process and attach the debugger to it...
+  }
 */
 
-	cancel(): void {
-		// in a "real" TestAdapter this would kill the child process for the current test run (if there is any)
-		throw new Error("Method not implemented.");
-	}
+  cancel(): void {
+    // in a "real" TestAdapter this would kill the child process for the current test run (if there is any)
+    throw new Error("Method not implemented.");
+  }
 
-	dispose(): void {
-		this.cancel();
-		for (const disposable of this.disposables) {
-			disposable.dispose();
-		}
-		this.disposables = [];
-	}
+  dispose(): void {
+    this.cancel();
+    for (const disposable of this.disposables) {
+      disposable.dispose();
+    }
+    this.disposables = [];
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
 import * as vscode from 'vscode';
 import { TestHub, testExplorerExtensionId } from 'vscode-test-adapter-api';
 import { Log, TestAdapterRegistrar } from 'vscode-test-adapter-util';
-import { ExampleAdapter } from './adapter';
+import { RubyAdapter } from './adapter';
 
 export async function activate(context: vscode.ExtensionContext) {
 
-	const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
+  const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
 
-	// create a simple logger that can be configured with the configuration variables
-	// `exampleExplorer.logpanel` and `exampleExplorer.logfile`
-	const log = new Log('exampleExplorer', workspaceFolder, 'Example Explorer Log');
-	context.subscriptions.push(log);
+  // create a simple logger that can be configured with the configuration variables
+  // `rubyExplorer.logpanel` and `rubyExplorer.logfile`
+  const log = new Log('rubyExplorer', workspaceFolder, 'Ruby Explorer Log');
+  context.subscriptions.push(log);
 
-	// get the Test Explorer extension
-	const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
-	if (log.enabled) log.info(`Test Explorer ${testExplorerExtension ? '' : 'not '}found`);
+  // get the Test Explorer extension
+  const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
+  if (log.enabled) log.info(`Test Explorer ${testExplorerExtension ? '' : 'not '}found`);
 
-	if (testExplorerExtension) {
+  if (testExplorerExtension) {
 
-		const testHub = testExplorerExtension.exports;
+    const testHub = testExplorerExtension.exports;
 
-		// this will register an ExampleTestAdapter for each WorkspaceFolder
-		context.subscriptions.push(new TestAdapterRegistrar(
-			testHub,
-			workspaceFolder => new ExampleAdapter(workspaceFolder, log),
-			log
-		));
-	}
+    // this will register an RubyTestAdapter for each WorkspaceFolder
+    context.subscriptions.push(new TestAdapterRegistrar(
+      testHub,
+      workspaceFolder => new RubyAdapter(workspaceFolder, log),
+      log
+    ));
+  }
 }
