19a50747e208de2c913f44bbe94a4f56e44006ec
Tabby Cromarty <tabby@brightmoon.online>
2022-02-19 19:04:56 +0000

    Remove leading _ from variables

--- src/frameworkDetector.ts
+++ src/frameworkDetector.ts
@@ -2,22 +2,22 @@ import * as vscode from 'vscode';
 import * as childProcess from 'child_process';
 import { IVSCodeExtLogger } from '@vscode-logging/logger';
 
-export function getTestFramework(_log: IVSCodeExtLogger): string {
-  let testFramework: string = (vscode.workspace.getConfiguration('rubyTestExplorer', null).get('testFramework') as string);
+export function getTestFramework(log: IVSCodeExtLogger): string {
+  let testFramework: string = vscode.workspace.getConfiguration('rubyTestExplorer', null).get('testFramework') || '';
   // If the test framework is something other than auto, return the value.
   if (['rspec', 'minitest', 'none'].includes(testFramework)) {
     return testFramework;
     // If the test framework is auto, we need to try to detect the test framework type.
   } else {
-    return detectTestFramework(_log);
+    return detectTestFramework(log);
   }
 }
 
 /**
  * Detect the current test framework using 'bundle list'.
  */
-function detectTestFramework(_log: IVSCodeExtLogger): string {
-  _log.info(`Getting a list of Bundler dependencies with 'bundle list'.`);
+function detectTestFramework(log: IVSCodeExtLogger): string {
+  log.info(`Getting a list of Bundler dependencies with 'bundle list'.`);
 
   const execArgs: childProcess.ExecOptions = {
     cwd: (vscode.workspace.workspaceFolders || [])[0].uri.fsPath,
@@ -30,8 +30,8 @@ function detectTestFramework(_log: IVSCodeExtLogger): string {
     let err, stdout = childProcess.execSync('bundle list', execArgs);
 
     if (err) {
-      _log.error(`Error while listing Bundler dependencies: ${err}`);
-      _log.error(`Output: ${stdout}`);
+      log.error(`Error while listing Bundler dependencies: ${err}`);
+      log.error(`Output: ${stdout}`);
       throw err;
     }
 
@@ -40,17 +40,17 @@ function detectTestFramework(_log: IVSCodeExtLogger): string {
     // Search for rspec or minitest in the output of 'bundle list'.
     // The search function returns the index where the string is found, or -1 otherwise.
     if (bundlerList.search('rspec-core') >= 0) {
-      _log.info(`Detected RSpec test framework.`);
+      log.info(`Detected RSpec test framework.`);
       return 'rspec';
     } else if (bundlerList.search('minitest') >= 0) {
-      _log.info(`Detected Minitest test framework.`);
+      log.info(`Detected Minitest test framework.`);
       return 'minitest';
     } else {
-      _log.info(`Unable to automatically detect a test framework.`);
+      log.info(`Unable to automatically detect a test framework.`);
       return 'none';
     }
   } catch (error: any) {
-    _log.error(error);
+    log.error(error);
     return 'none';
   }
 }
