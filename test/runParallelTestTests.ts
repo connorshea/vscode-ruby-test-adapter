4035eeb01b66e47d10837a56da2799a3297d22bb
lawrencegripper <info@grippers.co.uk>
2023-05-18 12:57:10 +0000

    Create test for parallel_tests

---
+++ test/runParallelTestTests.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+import * as cp from 'child_process';
+
+import { runTests, downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
+
+async function main() {
+  try {
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('stable')
+
+    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath)
+    cp.spawnSync(cliPath, ['--install-extension', 'hbenl.vscode-test-explorer'], {
+      encoding: 'utf-8',
+      stdio: 'inherit'
+    })
+
+    await runTests(
+      {
+        extensionDevelopmentPath,
+        extensionTestsPath: path.resolve(__dirname, './suite/frameworks/rspec/index'),
+        launchArgs: [path.resolve(extensionDevelopmentPath, 'test/fixtures/parallel_tests')]
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
+}
+
+main();
